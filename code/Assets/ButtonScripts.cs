b5cfa4950ea2c282c685d5ea61b24539be899289
st <tanuja.s@dell.com>
2019-03-08 13:25:32 +0530

    upload code with libraries

---
+++ code/Assets/ButtonScripts.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void ContactUs ()
+    {
+        Application.OpenURL("https://www.delltechnologies.com/en-us/index.htm");
+    }
+
+}
